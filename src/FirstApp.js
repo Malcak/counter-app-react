f68aaa25ac9419e940826b5486c45a04b4eee72c
Malcak <malcakx@gmail.com>
2021-05-26 05:32:15 -0500

    The response from the FirstApp component was wrapped in a fragment

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const FirstApp = () => {
-  return <h1>Hola mundo</h1>;
+  return (
+    <>
+      <h1>Hola mundo</h1>
+      <p>Mi primera aplicación</p>
+    </>
+  );
 };
 
 export default FirstApp;
