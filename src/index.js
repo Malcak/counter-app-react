0b111da74d5018657639951889d3856cf35f5a3f
Malcak <malcakx@gmail.com>
2021-05-26 05:25:48 -0500

    added index.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import FirstApp from './FirstApp';
 
+import './index.css';
+
 const divRoot = document.querySelector('#root');
 
 ReactDom.render(<FirstApp />, divRoot);
