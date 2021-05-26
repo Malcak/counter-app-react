75f9b263113eaa50db386f8a0c77d9bbfed81f1c
Malcak <malcakx@gmail.com>
2021-05-26 06:52:32 -0500

    added subtract and reset to counterapp component

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,12 +2,20 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(value);
 
   // handleAdd
   const handleAdd = () => {
-    // setCounter(counter + 1);
-    setCounter((c) => c + 1);
+    // setCounter((c) => c + 1);
+    setCounter(counter + 1);
+  };
+
+  const handleSubtract = () => {
+    setCounter(counter - 1);
+  };
+
+  const handleReset = () => {
+    setCounter(value);
   };
 
   return (
@@ -16,6 +24,8 @@ const CounterApp = ({ value }) => {
       <h2>{counter}</h2>
 
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleReset}>reset</button>
+      <button onClick={handleSubtract}>-1</button>
     </>
   );
 };
