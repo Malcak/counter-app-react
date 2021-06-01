3c5674a21e773e3279a5508cc980ffd6d2c941fc
Malcak <malcakx@gmail.com>
2021-06-01 04:50:42 -0500

    Added tests on the actions of the <CounterApp /> component

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -23,9 +23,15 @@ const CounterApp = ({ value }) => {
       <h1>CounterApp</h1>
       <h2 id="counterValue">{counter}</h2>
 
-      <button onClick={handleAdd}>+1</button>
-      <button onClick={handleReset}>reset</button>
-      <button onClick={handleSubtract}>-1</button>
+      <button id="add" onClick={handleAdd}>
+        +1
+      </button>
+      <button id="reset" onClick={handleReset}>
+        reset
+      </button>
+      <button id="subtract" onClick={handleSubtract}>
+        -1
+      </button>
     </>
   );
 };
--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -4,8 +4,11 @@ import { shallow } from 'enzyme';
 import CounterApp from '../CounterApp';
 
 describe('Test in <CounterApp /> component', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<CounterApp />);
+  });
   test('should display the <CounterApp /> component correctly', () => {
-    const wrapper = shallow(<CounterApp />);
     expect(wrapper).toMatchSnapshot();
   });
   test('should show the default value of 100 sent in in the properties', () => {
@@ -13,5 +16,23 @@ describe('Test in <CounterApp /> component', () => {
     const wrapper = shallow(<CounterApp value={value} />);
     const counterValue = wrapper.find('#counterValue').text();
     expect(counterValue).toBe('100');
+  });
+  test('should increment the counter when the add button is pressed', () => {
+    wrapper.find('#add').simulate('click');
+    const counterValue = wrapper.find('#counterValue').text();
+    expect(counterValue).toBe('1');
+  });
+  test('should reset the counter when the reset button is pressed', () => {
+    const value = 100;
+    const wrapper = shallow(<CounterApp value={value} />);
+    wrapper.find('#add').simulate('click');
+    wrapper.find('#reset').simulate('click');
+    const counterValue = wrapper.find('#counterValue').text();
+    expect(counterValue).toBe(value.toString());
+  });
+  test('should decrement the counter when the subtract button is pressed', () => {
+    wrapper.find('#subtract').simulate('click');
+    const counterValue = wrapper.find('#counterValue').text();
+    expect(counterValue).toBe('-1');
   });
 });
